66ce865f0ba6d05b5f9f2a2e06e8cbc2cdcb51bf
Haris-Irfan <harisirfan@gmail.com>
2025-05-12 23:24:11 +0800

    Create reddit datasets

--- scripts/Parsers/reddit-parser.py
+++ scripts/Parsers/reddit-parser.py
@@ -7,7 +7,7 @@ reddit = praw.Reddit(user_agent = True, client_id = "lRdHZehU8QxMGQzIvSYLiQ",
                      username = "H2REBE2R", password = "b2n2n2432")
 
 # Get subreddit instance. Change this field to the subreddit you want to scrape.
-subreddit = reddit.subreddit("relationship_advice")
+subreddit = reddit.subreddit("WorkAdvice")
 
 # Initialize empty list to store posts
 posts = []
@@ -18,7 +18,7 @@ for submission in subreddit.hot(limit=10):
     if submission.stickied:
         continue
 
-    if submission.author is None or submission.author.name == "Justkeepitanonymous":
+    if submission.author is None or submission.author.name == "Justkeepitanonymous" or submission.author.name == "AutoModerator":
         continue
 
     # Append the submission title and URL to the posts list
@@ -51,5 +51,5 @@ for submission in subreddit.hot(limit=10):
 
 # Save the posts to a JSON file, change the final path to the json file you want
 # to create.
-with open("../../data/raw/reddit/relationship_advice.json", "w") as f:
+with open("../../data/raw/reddit/WorkAdvice.json", "w") as f:
     json.dump(posts, f, indent=4)
